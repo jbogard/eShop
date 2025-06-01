14ddd9f76adb604a44d893b972bf7d0715a73984
Jimmy Bogard <jimmy.bogard@gmail.com>
2025-06-01 15:38:38 -0500

    Fixing quantity

--- src/Ordering.API/Apis/OrdersApi.cs
+++ src/Ordering.API/Apis/OrdersApi.cs
@@ -154,7 +154,7 @@ public static class OrdersApi
     }
 
     public static async Task<Results<Ok, BadRequest<string>>> CreateOrderAsync(
-        NewOrderModel model,
+        CreateOrderRequest model,
         [AsParameters] OrderServices services)
     {
         var address = new Address
@@ -168,8 +168,8 @@ public static class OrdersApi
         var order = new Order { OrderStatus = OrderStatus.Submitted, OrderDate = DateTime.UtcNow, Address = address };
         foreach (var item in model.Items)
         {
-            OrderManager.AddOrderItem(order, item.ProductId, item.ProductName, item.UnitPrice, item.Discount,
-                item.PictureUrl);
+            OrderManager.AddOrderItem(order, item.ProductId, item.ProductName, item.UnitPrice, 0m,
+                item.PictureUrl, item.Quantity);
         }
 
         await services.DbContext.Orders.AddAsync(order);
@@ -227,3 +227,19 @@ public static class OrdersApi
         return TypedResults.Ok();
     }
 }
+
+public record CreateOrderRequest(
+    string UserId,
+    string UserName,
+    string City,
+    string Street,
+    string State,
+    string Country,
+    string ZipCode,
+    string CardNumber,
+    string CardHolderName,
+    DateTime CardExpiration,
+    string CardSecurityNumber,
+    int CardTypeId,
+    string Buyer,
+    List<BasketItem> Items);
